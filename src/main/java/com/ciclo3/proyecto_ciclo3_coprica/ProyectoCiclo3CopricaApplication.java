98e9fa60ce4a0254a99e37a19fa50491d113de75
Andres Devia <pipedevia22@gmail.com>
2022-09-01 00:57:09 -0500

    Hola Mundo!

--- src/main/java/com/ciclo3/proyecto_ciclo3_coprica/ProyectoCiclo3CopricaApplication.java
+++ src/main/java/com/ciclo3/proyecto_ciclo3_coprica/ProyectoCiclo3CopricaApplication.java
@@ -2,10 +2,18 @@ package com.ciclo3.proyecto_ciclo3_coprica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class ProyectoCiclo3CopricaApplication {
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hola ciclo 3";
+    }
     public static void main(String[] args) {
         SpringApplication.run(ProyectoCiclo3CopricaApplication.class, args);
     }
