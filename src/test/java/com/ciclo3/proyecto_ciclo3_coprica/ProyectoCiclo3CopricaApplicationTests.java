2c6c430be2ae3388fd513e5b146795b0509c6606
Andres Devia <pipedevia22@gmail.com>
2022-09-01 00:22:16 -0500

    Primer commit

---
+++ src/test/java/com/ciclo3/proyecto_ciclo3_coprica/ProyectoCiclo3CopricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3.proyecto_ciclo3_coprica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCiclo3CopricaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
