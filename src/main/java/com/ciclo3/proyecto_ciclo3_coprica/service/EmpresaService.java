5113cf90abd2073aff9b851cf79e57c420ec559f
Andres Devia <pipedevia22@gmail.com>
2022-09-02 16:17:37 -0500

    Creacion del servicio de la entidad EmpresaService

--- src/main/java/com/ciclo3/proyecto_ciclo3_coprica/service/EmpresaService.java
+++ src/main/java/com/ciclo3/proyecto_ciclo3_coprica/service/EmpresaService.java
@@ -1,2 +1,37 @@
-package com.ciclo3.proyecto_ciclo3_coprica.service;public class EmpresaService {
+package com.ciclo3.proyecto_ciclo3_coprica.service;
+
+import com.ciclo3.proyecto_ciclo3_coprica.entidades.Empresa;
+import com.ciclo3.proyecto_ciclo3_coprica.repositorio.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmpresaService {
+    @Autowired // Conectamos esta clase con el repository de Empresa
+    EmpresaRepository empresaRepository;
+
+    //Metodo que me lista todas las empresas
+    public List<Empresa> getAllEmpresas(){
+        List<Empresa> empresaList = new ArrayList<>();
+        empresaRepository.findAll().forEach(empresa -> empresaList.add(empresa));
+        return empresaList;
+    }
+    //Metodo que me trae una sola empresa por su Id
+    public Empresa getEmpresaById(Integer id){
+
+        return empresaRepository.findById(id).get();
+    }
+
+    //Metodo para guardar o actualizar una empresa
+    public boolean saveOrUpdateEmpresa(Empresa empresa){
+        Empresa emp = empresaRepository.save(empresa);
+
+        if(empresaRepository.findById(emp.getId()) !=null){
+            return true;
+        }
+        return false;
+    }
 }
