3697b62c49f7689c38b8acdad8ad689836924584
Andres Devia <pipedevia22@gmail.com>
2022-09-02 16:16:43 -0500

    Creacion del repositorio de la entidad EmpresaRepository

--- src/main/java/com/ciclo3/proyecto_ciclo3_coprica/repositorio/EmpresaRepository.java
+++ src/main/java/com/ciclo3/proyecto_ciclo3_coprica/repositorio/EmpresaRepository.java
@@ -1,2 +1,9 @@
-package com.ciclo3.proyecto_ciclo3_coprica.repositorio;public interface EmpresaRepository {
+package com.ciclo3.proyecto_ciclo3_coprica.repositorio;
+
+import com.ciclo3.proyecto_ciclo3_coprica.entidades.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
 }
