8a07f5dbfb5308f42dfa8cd988edc5f4342749e3
Andres Devia <pipedevia22@gmail.com>
2022-09-09 23:48:41 -0500

    Se modifico los complementos de aplicacion

--- src/main/java/com/ciclo3/proyecto_ciclo3_coprica/ProyectoCiclo3CopricaApplication.java
+++ src/main/java/com/ciclo3/proyecto_ciclo3_coprica/ProyectoCiclo3CopricaApplication.java
@@ -1,27 +1,11 @@
 package com.ciclo3.proyecto_ciclo3_coprica;
 
-import com.ciclo3.proyecto_ciclo3_coprica.entidades.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class ProyectoCiclo3CopricaApplication {
-
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hola ciclo 3";
-    }
-
-    @GetMapping("/test")
-    public String test(){
-        Empresa emp = new Empresa("GESAMFOR","CAlle 17", "3007345956", "58558585-3");
-        emp.setNombre("SOLAR LTDA");
-        return emp.getNombre();
-    }
     public static void main(String[] args) {
         SpringApplication.run(ProyectoCiclo3CopricaApplication.class, args);
     }
