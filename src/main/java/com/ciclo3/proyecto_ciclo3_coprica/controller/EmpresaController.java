1fb5918e481153fe2f3a7a686bad772a53ca4990
Andres Devia <pipedevia22@gmail.com>
2022-09-09 23:47:07 -0500

    Se crearon los controladores de cada servicio

--- src/main/java/com/ciclo3/proyecto_ciclo3_coprica/controller/EmpresaController.java
+++ src/main/java/com/ciclo3/proyecto_ciclo3_coprica/controller/EmpresaController.java
@@ -1,2 +1,53 @@
-package com.ciclo3.proyecto_ciclo3_coprica.controller;public class EmpresaController {
+package com.ciclo3.proyecto_ciclo3_coprica.controller;
+
+import com.ciclo3.proyecto_ciclo3_coprica.entidades.Empresa;
+import com.ciclo3.proyecto_ciclo3_coprica.service.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmpresaController {
+    @Autowired
+    EmpresaService empresaService;
+
+    //Listamos todas la empresas registradas
+    @GetMapping("/enterprises")
+    public List<Empresa> verEmpresas() {
+        return empresaService.getAllEmpresas();
+    }
+
+    //Guardamos o actualizamos una empresa
+    @PostMapping("/enterprises")
+    public Empresa guardarEmpresa(@RequestBody Empresa emp) {
+        return this.empresaService.saveOrUpdateEmpresa(emp);
+    }
+
+    @GetMapping(path = "enterprises/{id}")
+    public Empresa empresaPorID(@PathVariable("id") Integer id) {
+        return this.empresaService.getEmpresaById(id);
+    }
+
+    @PatchMapping("/enterprises/{id}")
+    public Empresa actualizarEmpresa(@PathVariable("id") Integer id, @RequestBody Empresa empresa) {
+        Empresa emp = empresaService.getEmpresaById(id);
+        emp.setNombre(empresa.getNombre());
+        emp.setDireccion(empresa.getDireccion());
+        emp.setTelefono(empresa.getTelefono());
+        emp.setNIT(empresa.getNIT());
+
+        return empresaService.saveOrUpdateEmpresa(emp);
+    }
+
+    @DeleteMapping(path = "enterprises/{id}")//Eliminar una empresa de la base de datos
+    public String eliminarEmpresa(@PathVariable("id") Integer id) {
+        boolean respuesta = this.empresaService.deleteEmpresa(id);
+        if (respuesta){
+            return "Empresa con el Id " + id + " Eliminada Correctamente";
+        }else {
+            return "Empresa con el Id: "+id+ " No pudo ser eliminada";
+        }
+
+    }
 }
